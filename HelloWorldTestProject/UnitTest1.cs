3728de13c08dc23b1b5e9363ce4a191d29c8d8cb
Richard Louapre <richard.louapre@gmail.com>
2012-11-09 14:37:08 -0500

    Initial commit.

---
+++ HelloWorldTestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelloWorldTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ClassLibrary1.Class1 klass = new ClassLibrary1.Class1();
+            string helloWorld = klass.GetHelloWorld();
+            Assert.AreEqual("Hello world", helloWorld);
+        }
+    }
+}
