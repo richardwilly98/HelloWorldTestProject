5c6c287fb6a7bddb1336121da35d82ad2d0df809
Richard Louapre <richard.louapre@gmail.com>
2012-11-09 14:41:07 -0500

    New method.

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
+        public string GetHelloWorld(string user)
+        {
+            return String.Format("Hello {0}", user);
+        }
+
         public string GetHelloWorld()
         {
             return "Hello world";
